0d9e12c0d3ecfb8f2259ed19bdedc2395a0b374a
BlackPeace5 <57791853+BlackPeace5@users.noreply.github.com>
2020-04-15 15:56:46 +0300

    Fix bag

--- MyLog.php
+++ MyLog.php
@@ -14,13 +14,13 @@ Class MyLog extends LogAbstract implements LogInterface {
 	public static function log($str){
 		MyLog::Instance()->log[] = $str;
 	}
-	
+
 	public function _write() {
-	
+
 		$d = new \DateTime("NOW");
-		$version = file_get_contents(__DIR__. "/../version");
-		if (!file_exists(__DIR__. "/../log")) { 
-			mkdir(__DIR__. "/../log", 0777, true);
+		$version = file_get_contents(__DIR__. "/../../../version");
+		if (!file_exists(__DIR__. "/../../../log")) {
+			mkdir(__DIR__. "/../../../log", 0777, true);
 		}
 		file_put_contents(BASEURI . '/log/' . $d->format('d-m-Y__H__i_s_u') . '.log', "File Version: " . $version . "\r\n" . implode("\r\n", MyLog::Instance()->log));
 		echo implode("\r\n", MyLog::Instance()->log);
